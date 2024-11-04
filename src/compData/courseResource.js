697703920a5ebea013179f02e8e090dc7df98884
绿苗 <lvmiao233@outlook.com>
2024-11-04 14:41:59 +0800

    feat(asset): add Ch5 slides

--- src/compData/courseResource.js
+++ src/compData/courseResource.js
@@ -1,67 +1,73 @@
 const resourceData = [
     {
         "file_type": "pdf",
-        "name": "Computer Networks - 5th edition.pdf",
+        "name": "Computer Networks - 5th edition",
         "size": "8454231",
         "link": "http://10.214.0.253/network/exercise/courseware/upload/110/luxq_Computer%20Networks%20-%20A%20Tanenbaum%20-%205th%20edition.pdf"
     },
     {
         "file_type": "pdf",
-        "name": "1 Introduction.pdf",
+        "name": "1 Introduction",
         "size": "3822490",
         "link": require('@site/assets/slides/luxq_1_Introduction.pdf').default
     },
     {
         "file_type": "pdf",
-        "name": "2 Physical Layer.pdf",
+        "name": "2 Physical Layer",
         "size": "4249125",
         "link": require('@site/assets/slides/luxq_2_The Physical Layer.pdf').default
     },
     {
         "file_type": "pdf",
-        "name": "3 Data Link Layer.pdf",
+        "name": "3 Data Link Layer",
         "size": "5465726",
         "link": require('@site/assets/slides/luxq_3_Data Link Layer.pdf').default
     },
     {
         "file_type": "pdf",
-        "name": "4 Medium Access Control Sublayer.pdf",
+        "name": "4 Medium Access Control Sublayer",
         "size": "3401217",
         "link": require('@site/assets/slides/4_MACLayer.pdf').default
     },
     {
         "file_type": "pdf",
-        "name": "Lab1 Wireshark.pdf",
+        "name": "5 Network Layer",
+        "size": "7491147",
+        "link": require('@site/assets/slides/5_Network Layer.pdf').default
+    },
+    {
+        "file_type": "pdf",
+        "name": "Lab1 Wireshark",
         "size": "2874374",
         "link": require('@site/assets/slides/luxq_Wireshark.pdf').default
     },
     {
         "file_type": "pdf",
-        "name": "Lab2 使用二层交换机组网.pdf",
+        "name": "Lab2 使用二层交换机组网",
         "size": "1223654",
         "link": require('@site/assets/slides/luxq_Lab2.pdf').default
     },
     {
         "file_type": "pdf",
-        "name": "Lab3 使用三层交换机组网.pdf",
+        "name": "Lab3 使用三层交换机组网",
         "size": "650417",
         "link": require('@site/assets/slides/luxq_Lab3-使用三层交换机组网.pdf').default
     },
     {
         "file_type": "pdf",
-        "name": "Lab4-静态路由配置.pdf",
+        "name": "Lab4 静态路由配置",
         "size": "4490633",
         "link": require('@site/assets/slides/Lab4-静态路由配置.pdf').default
     },
     {
         "file_type": 'pdf',
-        "name":'GNS3模拟环境中使用WireShark抓STP协议报文.pdf',
+        "name":'GNS3模拟环境WireShark抓STP协议报文',
         "size":'1204626',
         "link": require('@site/assets/slides/GNS3模拟环境中使用WireShark抓STP协议报文.pdf').default
     },
     {
         "file_type": "pdf",
-        "name": "Lab7 基于Socket接口实现自定义协议通信.pdf",
+        "name": "Lab7 基于Socket接口实现自定义协议通信",
         "size": "1967383",
         "link": require('@site/assets/slides/luxq_Socket编程.pdf').default
     },
--- src/components/FileCard.js
+++ src/components/FileCard.js
@@ -46,7 +46,7 @@ const FileCard = (props) => {
         <a href={link} style={{textDecoration: 'none', color: 'inherit'}}>
             <div className="file-card">
                 <div style={{display: 'flex'}}>
-                    <div style={{width: '52px', minHeight: '100%'}}>
+                    <div style={{width: '52px', height: '48px', minHeight: '100%'}}>
                         <div style={{display: 'flex', alignItems: 'center', justifyContent: 'flex-start', height: '100%'}}>
                             {fileIcon}
                         </div>
