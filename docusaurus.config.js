741d70c5702518472cc9b156c32ea240aca75eec
绿苗 <lvmiao233@outlook.com>
2024-11-04 14:41:26 +0800

    feat(site): custom social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,13 +15,11 @@ const config = {
   // Set the production url of your site here
   url: 'https://zjucomp.net',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'lvmiao233', // Usually your GitHub org/user name.
-  projectName: 'ZJU24CompNet', // Usually your repo name.
+  organizationName: 'lvmiao233', // GitHub org/user name.
+  projectName: 'ZJU24CompNet', // repo name.
 
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
@@ -38,29 +36,11 @@ const config = {
     [
       '@docusaurus/plugin-client-redirects',
       {
-        // fromExtensions: ['html', 'htm'], // /myPage.html -> /myPage
-        // toExtensions: ['exe', 'zip'], // /myAsset -> /myAsset.zip (if latter exists)
         redirects: [
           // /docs/oldDoc -> /docs/newDoc
-          {
-            to: '/docs/Lab7_page',
-            from: '/docs/Coding/Lab7_page',
-          },
-          {
-            to: '/docs/Lab8_page',
-            from: '/docs/Coding/Lab8_page',
-          },
+          { to: '/docs/Lab7_page', from: '/docs/Coding/Lab7_page', },
+          { to: '/docs/Lab8_page', from: '/docs/Coding/Lab8_page', },
         ],
-        // createRedirects(existingPath) {
-        //   if (existingPath.includes('/community')) {
-        //     // Redirect from /docs/team/X to /community/X and /docs/support/X to /community/X
-        //     return [
-        //       existingPath.replace('/community', '/docs/team'),
-        //       existingPath.replace('/community', '/docs/support'),
-        //     ];
-        //   }
-        //   return undefined; // Return a falsy value: no redirect created
-        // },
       },
     ],
   ],
@@ -72,21 +52,15 @@ const config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Enable this to enable the "edit this page" links.
+          // editUrl: 'https://github.com/lvmiao233/ZJU24CompNet/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Enable this to enable the "edit this page" links.
+          // editUrl: 'https://github.com/lvmiao233/ZJU24CompNet/',
         },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
+        theme: { customCss: './src/css/custom.css', },
       }),
     ],
   ],
@@ -97,66 +71,34 @@ const config = {
       algolia: {
         // The application ID provided by Algolia
         appId: 'EGPMMA958K',
-
         // Public API key: it is safe to commit it
         apiKey: 'c5f86a0c2b668b32ac94f0e6761e124f',
-
         indexName: 'zjucomp',
-
         // Optional: see doc section below
         contextualSearch: true,
-
-        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-        // externalUrlRegex: 'external\\.com|domain\\.com',
-
-        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-        // replaceSearchResultPathname: {
-        //   from: '/docs/', // or as RegExp: /\/docs\//
-        //   to: '/',
-        // },
-
-        // Optional: Algolia search parameters
-        searchParameters: {},
-
         // Optional: path for search page that enabled by default (`false` to disable it)
         searchPagePath: 'search',
-
         // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
         insights: true,
-
-        //... other Algolia params
       },
-      metadata: [
-        {name: 'name', content: '浙大计网实验文档'},
-      ],
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      metadata: [ {name: 'name', content: '浙大计网实验文档'}, ],
+      image: 'img/zjucompnet-social-card.jpg',
       navbar: {
         title: '计算机网络课程实验',
         logo: { alt: '计算机网络课程实验', src: 'img/logo.svg', },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: '实验指导',
-          },
-          {
-            type: 'docSidebar',
-            sidebarId: 'noteSidebar',
-            position: 'left',
-            label: '课程解析',
-          },
-          {to: '/blog', label: '延伸阅读', position: 'left'},
-          {
+            type: 'docSidebar', sidebarId: 'tutorialSidebar',
+            label: '实验指导', position: 'left',
+          }, {
+            type: 'docSidebar', sidebarId: 'noteSidebar',
+            label: '课程解析', position: 'left',
+          }, {to: '/blog', label: '延伸阅读', position: 'left'}, {
             href: 'https://github.com/lvmiao233/NetLabFramework',
-            label: '测试框架',
-            position: 'right',
-          },
-          {
+            label: '测试框架', position: 'right',
+          }, {
             href: 'https://github.com/lvmiao233/ZJU24CompNet',
-            label: '文档仓库',
-            position: 'right',
+            label: '文档仓库', position: 'right',
           },
         ],
       },
@@ -170,35 +112,19 @@ const config = {
               { label: '课程解析', to: '/notes/intro/', },
               { label: '延伸阅读', to: '/blog/', },
             ],
-          },
-          {
+          }, {
             title: '其他课程资源',
             items: [
-              {
-                label: '计算机网络课程网站',
-                href: 'http://10.214.0.253/network/exercise/index.php',
-              }, {
-                label: '计算机网络朋辈辅学',
-                href: 'https://www.yuque.com/xianyuxuan/coding/network',
-              }, {
-                label: '浙江大学课程攻略共享计划',
-                href: 'https://github.com/QSCTech/zju-icicles',
-              },
+              { label: '计算机网络课程网站', href: 'http://10.214.0.253/network/exercise/index.php', },
+              { label: '计算机网络朋辈辅学', href: 'https://www.yuque.com/xianyuxuan/coding/network', },
+              { label: '浙江大学课程攻略共享计划', href: 'https://github.com/QSCTech/zju-icicles', },
             ],
-          },
-          {
+          }, {
             title: '实验资源',
             items: [
-              {
-                label: 'WireShark',
-                href: 'https://www.wireshark.org/',
-              }, {
-                label: 'GNS3',
-                href: 'https://github.com/GNS3/gns3-gui',
-              }, {
-                label: '编程实验测试框架',
-                href: 'https://github.com/lvmiao233/NetLabFramework',
-              },
+              { label: 'WireShark', href: 'https://www.wireshark.org/', },
+              { label: 'GNS3', href: 'https://github.com/GNS3/gns3-gui', },
+              { label: '编程实验测试框架', href: 'https://github.com/lvmiao233/NetLabFramework', },
             ],
           },
         ],
@@ -209,11 +135,7 @@ const config = {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
       },
-      docs: {
-        sidebar: {
-          hideable: true,
-        },
-      },
+      docs: { sidebar: { hideable: true, }, },
     }),
 };
 
